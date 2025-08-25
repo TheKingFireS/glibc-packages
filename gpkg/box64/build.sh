010b3d5d69201e6c218251d1459524a3a8f84aae
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2025-08-25 06:29:12 +0200

    build.sh: bump to latest master

--- gpkg/box64/build.sh
+++ gpkg/box64/build.sh
@@ -3,13 +3,12 @@ TERMUX_PKG_DESCRIPTION="Linux Userspace x86_64 Emulator with a twist"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=0.3.7
-TERMUX_PKG_SRCURL=https://github.com/ptitSeb/box64/archive/df2e4f941f616a775388004138e605518d9cdb8d.tar.gz
-TERMUX_PKG_SHA256=147af101d74d58b7e28df4c4f63d1b248993924cf2e9af4b9e64cab42952262a
+TERMUX_PKG_SRCURL=https://github.com/ptitSeb/box64/archive/747882cb4ef5d90955de8b21f74b9fc7850cc39c.tar.gz
+TERMUX_PKG_SHA256=81c44fd0f34bdaf9b30d7ec2825805cccc3de29b677cde5b34334232359ffe6e
 TERMUX_PKG_DEPENDS="gcc-libs-glibc"
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DBOX32=ON -DBOX32_BINFMT=ON"
-TERMUX_PKG_RM_AFTER_INSTALL="glibc/etc/binfmt.d"
 
 termux_step_pre_configure() {
 	if [ "${TERMUX_ARCH}" = "aarch64" ]; then
